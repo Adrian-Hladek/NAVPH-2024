56bcb76c3ad2d795bd0914bd3a2d6effa8a6d30a
Adrián Hládek <71824498+Adrian-Hladek@users.noreply.github.com>
2024-12-21 19:53:15 +0100

    Edits to sprites

--- First_Game_Best_Game/Assets/Scripts/UI/MainMenu.cs
+++ First_Game_Best_Game/Assets/Scripts/UI/MainMenu.cs
@@ -6,8 +6,27 @@ using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class MainMenu : MonoBehaviour
 {
-   
-    public void  PlayGame()
+
+    public void PlayLevel1()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+
+    }
+
+    public void PlayLevel2()
+    {
+
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+
+    }
+
+    public void PlayLevel3()
     {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
